080f1a435e088ef4d74568c1f93a85c4c04caead
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2022-07-19 13:18:54 -0400

    first commit

--- ocdownload.py
+++ ocdownload.py
@@ -1,6 +1,10 @@
 import re
 import argparse
 from dotenv import load_dotenv
+import shutil
+from subprocess import run
+
+
 
 import os
 import subprocess
@@ -15,8 +19,8 @@ WORKDIR = os.getenv('WORKDIR', default=home)
 
 parser = argparse.ArgumentParser(description='Downloads ocp release')
 parser.add_argument('--ocpversion', type=str, required=True)
+parser.add_argument('--registryurl', type=str, required=True)
 args = parser.parse_args()
-
 
 channel = args.ocpversion.rsplit('.', 1)[0]
 
@@ -24,7 +28,16 @@ parameter_values_dict = {"<ocpchannel>" :channel, "<ocpversion>" :args.ocpversio
 
 ocp4path = f'{WORKDIR}/download/ocp4'
 if not os.path.isdir(ocp4path):
-    os.makedirs (ocp4path)
+    shutil.rmtree(ocp4path)
+    os.makedirs(ocp4path)
 
 Utilities.replaceInFile("templates/imageset-config-ocp4.yaml", f'{ocp4path}/imageset-config.yaml', parameter_values_dict)
 print(f'{ocp4path}/imageset-config.yaml')
+print('running oc-mirror')
+data = run(['oc-mirror', '--dry-run', '--config=./imageset-config.yaml', args.registryurl],capture_output=True,shell=True)
+print(data.stdout)
+print(data.stderr)
+
+#oc-mirror     docker://registry.swarchpoc.com
+
+
