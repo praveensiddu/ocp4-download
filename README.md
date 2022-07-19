5c450f57db28a48b9050eed8f77bfdc079ede46c
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2022-07-19 03:44:42 -0400

    first commit

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 # Host prep steps
 
-1. download oc-mirror
+1. Install software
+```
+sudo dnf install python3-pip
+sudo pip3 install python-dotenv
+```
+
+3. download oc-mirror
 ``` 
 python oc-mirror-download.py
 ```
--- ocdownload.py
+++ ocdownload.py
@@ -8,7 +8,10 @@ from pathlib import Path
 from common.utilities import Utilities
 
 load_dotenv()
-WORKDIR = os.getenv('WORKDIR')
+home = str(Path.home())
+WORKDIR = os.getenv('WORKDIR', default=home)
+
+
 
 parser = argparse.ArgumentParser(description='Downloads ocp release')
 parser.add_argument('--ocpversion', type=str, required=True)
@@ -22,5 +25,5 @@ input_text = "some text @@ocpchannel@@ some more text @@ocpversion@@ some extra 
 output_text = re.sub(r"@@(\w+?)@@", lambda match: parameter_values_dict[match.group(1)], input_text)
 print(output_text)
 
-Utilities.replaceInFile("templates/imageset-config-ocp4.yaml", f'{WORKDIR}/imageset-config.yaml', parameter_values_dict)
+Utilities.replaceInFile("templates/imageset-config-ocp4.yaml", f'{WORKDIR}/download/imageset-config.yaml', parameter_values_dict)
 
